f9506a7ccbae7f8fa2959f5cd8627b4aef1bf316
screeeen <miguel@screenimplosion.com>
2020-01-10 15:15:03 +0100

    first solution

--- spec/fizzbuzzSpec.js
+++ spec/fizzbuzzSpec.js
@@ -5,7 +5,22 @@ FOR TEST EXAMPLES AND DIFFERENT MATCHERS (`toEqual`, `toBe`, `not.toBe`, etc):
 */
 
 describe("convert function", function () { // Describes the suite / group of tests
-  it("Returns undefined when parameter passed is a string", function () {
+  it("convert an integer in its value as a string", function () {
     expect(convert(1)).toEqual("1");
+  });
+  it("convert an integer in its value as a string", function () {
+    expect(convert(2)).toEqual("2");
+  });
+  it("convert an integer in its value as a string", function () {
+    expect(convert(4)).toEqual("4");
+  });
+  it("convert an integer in its value as a string, if the integer is 3, then return the string Fizz", function () {
+    expect(convert(3)).toEqual("Fizz");
+  });
+  it("convert an integer in its value as a string, if the integer is 5, then return the string Buzz", function () {
+    expect(convert(5)).toEqual("Buzz");
+  });
+  it("convert an integer in its value as a string, if the integer is 15, then return the string FizzBuzz", function () {
+    expect(convert(15)).toEqual("FizzBuzz");
   });
 });
--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,3 +1,21 @@
-    function convert(n) {
-        return "1";
+    function convert(i) {
+
+      if (calcModuleOfThreeAndfive(i) == 0) return "FizzBuzz";
+      if (calcModuleOfThree(i) == 0) return "Fizz";
+      if (calcModuleOfFive(i) == 0) return "Buzz";
+
+        return String(i);
+    }
+
+    function calcModuleOfThree (numberToCalc){
+      return numberToCalc % 3;
+    }
+
+    function calcModuleOfFive (numberToCalc){
+      return numberToCalc % 5;
+    }
+
+    function calcModuleOfThreeAndfive (numberToCalc){
+
+      return calcModuleOfThree(numberToCalc) + calcModuleOfFive(numberToCalc);
     }
